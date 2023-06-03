ba7475d99042413bbcf3b869c7717a0c55ca572f
escalopa <ahmad.helaly.dev@gmail.com>
2023-06-03 17:58:24 +0300

    build(scripts): create deploy scripts & db init

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# this scripts builds and tags with docker
+
+# it takes one argument, the tag version
+
+TAG=latest
+
+if [ $# -eq 1 ]; then
+    TAG=$1
+fi
+
+docker build -t dekuyo/chatmap:$TAG -f ./Dockerfile --target production .
+docker push dekuyo/chatmap:$TAG
