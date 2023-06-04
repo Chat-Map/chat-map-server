d1dd85a2b9c2b79c3245463bdbb9fd68547a2a0c
escalopa <ahmad.helaly.dev@gmail.com>
2023-06-04 15:40:31 +0300

    feat(hasher): create bcrypt password hasher

---
+++ internal/adapters/hasher/bcrypt.go
@@ -0,0 +1,31 @@
+package hasher
+
+import (
+	"context"
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+const minPasswordLen = 8
+
+type BcryptHasher struct{}
+
+func NewBcryptHasher() *BcryptHasher {
+	return &BcryptHasher{}
+}
+
+func (c *BcryptHasher) Hash(ctx context.Context, password string) (string, error) {
+	if password == "" {
+		return "", fmt.Errorf("password length is less than %d", minPasswordLen)
+	}
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %w", err)
+	}
+	return string(hash), nil
+}
+
+func (c *BcryptHasher) Compare(ctx context.Context, hash, password string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) == nil
+}
