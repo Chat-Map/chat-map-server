53132d3efbfbc07cfe4c12989f91d77e849053c3
escalopa <ahmad.helaly.dev@gmail.com>
2023-06-04 20:06:13 +0300

    feat(db): impl get user by email

--- internal/adapters/db/postgres/query/users.sql
+++ internal/adapters/db/postgres/query/users.sql
@@ -3,10 +3,15 @@ INSERT INTO users (first_name, last_name, email, password)
 VALUES ($1, $2, $3, $4)
 RETURNING id;
 
--- name: GetUser :one
+-- name: GetUserByID :one
 SELECT *
 FROM users
 WHERE id = $1;
+
+-- name: GetUserByEmail :one
+SELECT *
+FROM users
+WHERE email = $1;
 
 -- name: SearchUserByEmail :many
 SELECT *
